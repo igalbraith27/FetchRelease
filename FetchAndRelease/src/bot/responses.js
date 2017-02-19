c90b99482733a94d661c588a30833c638f0fb353
igalbraith27 <igalbraith27@berkeley.edu>
2017-02-18 18:31:20 -0800

    Necessary files

---
+++ FetchAndRelease/src/bot/responses.js
@@ -0,0 +1,8 @@
+export default {
+  'Hello': 'Hey!',
+  'Hi':'Hey!',
+  'hello':'Hey!',
+  'hi':'Hey!',
+  "What's your name?": 'I am the Fetch and Release Bot!',
+  "Are you a robot?": "no."
+}
---
+++ FetchAndRelease/src/config.js
@@ -0,0 +1,6 @@
+var messengerPageToken = process.env.MESSENGER_TOKEN || 'EAAaXfMDide4BADRnMEbE4x242r9hFhu00FxXWV3Ftm7UfOWJtCApMs8ZCUgcpYyhp2RqqhfkxbXGf3XTBQZA4Qh1UAp9tVZCeR4xkPZBOMxEMtJdlwkBpOga5WRlmGvTyNnHhOeUctlLZCtKiTYFPYnfZC291JfIzZASUUZBWvdZCZBwZDZD';
+
+module.exports = {
+  messengerPageToken: messengerPageToken,
+  messengerVerifyToken: 'NikitNikitNikitNikit'
+};
